8fc529b4a610902e8851b4b750da13475541322d
Wisnu Mulya <wisnu@wisnumulya.com>
2021-06-12 00:47:39 +0700

    Added ETL code to etl.py

--- etl.py
+++ etl.py
@@ -6,38 +6,50 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """Process song file informations into songs and artists tables
+    
+    Args:
+        cur: psycopg2 cursor object to execute queries to the database
+        filepath: a string of the filepath where the JSON song file is
+    """
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = list(df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0])
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = list(df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0])
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
+    """Process log file informations into time, users, and songplays tables
+    
+    Args:
+        cur: psycopg2 cursor object to execute queries to the database
+        filepath: a string of the filepath where the JSON log file is
+    """
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    t = df['ts'].apply(lambda x: pd.to_datetime(x, unit='ms'))
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = zip(df['ts'], t.dt.hour, t.dt.day, t.dt.isocalendar().week, t.dt.month, t.dt.year, t.dt.weekday)
+    column_labels = ('ts', 'hour', 'day', 'week', 'month', 'year', 'weekday')
+    time_df = pd.DataFrame(time_data, columns=column_labels)
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']].drop_duplicates()
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -56,11 +68,26 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (row['ts'],
+                         row['userId'],
+                         row['level'],
+                         songid,
+                         artistid,
+                         row['sessionId'],
+                         row['location'],
+                         row['userAgent'])
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
+    """Process song or log data to load into sparkifydb database
+
+    Args:
+        cur: psycopg2 cursor object to execute queries to the database
+        conn: psycopg2 connection object to the database
+        filepath: filepath to the data to be extracted
+        func: a function to process the data
+    """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -80,6 +107,7 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    """Run the ETL to process song and log files"""
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
